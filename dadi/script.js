eb0f55355b9ad52a370f836ab3509b75a62740c2
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-04 15:34:16 +0100

    feat: confronto numeri ed output

--- dadi/script.js
+++ dadi/script.js
@@ -3,3 +3,11 @@ console.log("Numero giocatore: " + numPlayer);
 
 const numComputer = Math.floor (Math.random() * 6) + 1;
 console.log("Numero computer: " + numComputer);
+
+if (numPlayer > numComputer) {
+    console.log("Player win!");
+} else if (numPlayer < numComputer) {
+    console.log("Computer win!");    
+} else {
+    console.log("Draw!");
+}
